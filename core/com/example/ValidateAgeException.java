402b2c9ce357b141a67b51975987f96c53bd606d
sagarshingare16 <sagargshingare16@gmail.com>
2024-09-08 21:14:58 +0530

    custom checked & unchcked exception

--- core/com/example/ValidateAgeException.java
+++ core/com/example/ValidateAgeException.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class ValidateAgeException extends Exception{
     public ValidateAgeException(){
-        super("Not eligible to vote!");
+        super("Not eligible to vote!!");
     }
     public ValidateAgeException(String message){
         super(message);
--- core/com/example/ValidateAgeRunTimeException.java
+++ core/com/example/ValidateAgeRunTimeException.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class ValidateAgeRunTimeException extends RuntimeException{
     public ValidateAgeRunTimeException(){
-        super("Not eligible!");
+        super("Not eligible!!");
     }
     public ValidateAgeRunTimeException(String message){
         super("Not eligible");
