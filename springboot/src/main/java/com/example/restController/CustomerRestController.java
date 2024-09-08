4a5fa36bf13def30a98a2cd4471ff098734f82da
sagarshingare16 <sagargshingare16@gmail.com>
2024-09-08 21:13:26 +0530

    spring boot exception handling without global exception handler

--- springboot/src/main/java/com/example/restController/CustomerRestController.java
+++ springboot/src/main/java/com/example/restController/CustomerRestController.java
@@ -1,9 +1,16 @@
 package com.example.restController;
 
+import com.example.advice.ErrorResponse;
+import com.example.exception.CustomerAlreadyExistsException;
+import com.example.exception.NoSuchCustomerExistsException;
 import com.example.model.Customer;
 import com.example.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.context.request.WebRequest;
+
+import java.time.LocalDateTime;
 
 @RestController
 @RequestMapping("/customer")
@@ -26,4 +33,17 @@ public class CustomerRestController {
     public String updateCustomer(@RequestBody Customer customer) {
         return customerService.updateCustomer(customer);
     }
+
+    /* Exception Handling */
+    @ExceptionHandler(CustomerAlreadyExistsException.class)
+    public ErrorResponse handleCustomerAlreadyExistsException(CustomerAlreadyExistsException ex, WebRequest request) {
+        return new ErrorResponse(HttpStatus.CONFLICT.value(),
+                ex.getMessage(), LocalDateTime.now(), request.getDescription(false));
+    }
+    @ExceptionHandler(NoSuchCustomerExistsException.class)
+    public ErrorResponse handleNoSuchCustomerExistsException(NoSuchCustomerExistsException ex, WebRequest request) {
+        return new ErrorResponse(HttpStatus.NOT_FOUND.value(),
+                ex.getMessage(), LocalDateTime.now(), request.getDescription(false));
+    }
 }
+
