b5f3d66f577ae82043bc56e443a40b198b7dfd40
sagarshingare16 <sagargshingare16@gmail.com>
2024-09-08 14:53:03 +0530

    throws, throw & custom exception

--- core/com/example/ValidateAgeException.java
+++ core/com/example/ValidateAgeException.java
@@ -2,7 +2,7 @@ package com.example;
 
 public class ValidateAgeException extends RuntimeException{
     public ValidateAgeException(){
-
+        super("Not eligible to vote!");
     }
     public ValidateAgeException(String message){
         super(message);
